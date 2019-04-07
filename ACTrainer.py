f188b1c61d32f27efbfe8f54ebb0066dc90af2ea
Jeffrey Lin <afunpub@gmail.com>
2019-04-08 01:00:27 +0800

    start to develop ACTrainer

---
+++ ACTrainer.py
@@ -0,0 +1,12 @@
+from tkinter import Button, Entry, Tk, Label
+import ttk
+import tkfont
+import image
+import imageTk
+import tkMessageBox
+from ReadyWriteMemory import rwm
+
+
+class AC_Trainer:
+
+    def pass
