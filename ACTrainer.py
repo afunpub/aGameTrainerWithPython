8e59dedc4bb5ddc88f898fa833d71b0a23a3885c
Jeffrey Lin <afunpub@gmail.com>
2019-04-09 00:35:01 +0800

    set main method,height and width.

--- ACTrainer.py
+++ ACTrainer.py
@@ -10,3 +10,13 @@ from ReadyWriteMemory import rwm
 class AC_Trainer:
 
     def pass
+
+
+def main():
+    root = Tk()
+    wFilter = 307 // Filter視窗程式的寬與高
+    hFilter = 355
+    w = wFilter-8
+    h = hFilter-47
+    sw=root.winfo_screenwidth()
+    sh=root.winfo_screenheight()
